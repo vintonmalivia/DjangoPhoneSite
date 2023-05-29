fb8bf86eb3c90166c8d77f1b99a1e060377664a2
Anton Shvechikhin <antonkamski@gmail.com>
2023-05-29 23:31:20 +0400

    Using non-model forms

--- phones/views.py
+++ phones/views.py
@@ -1,5 +1,7 @@
 from django.http import *
 from django.shortcuts import *
+
+from phones.forms import *
 from phones.models import *
 
 # Create your views here.
@@ -27,7 +29,25 @@ def about(request):
 
 
 def addpage(request):
-    return HttpResponse('Добавление статьи')
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            try:
+                Phone.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+
+    else:
+        form = AddPostForm()
+
+    context = {
+        'menu': menu,
+        'title': 'Добавление статьи',
+        'form': form
+    }
+    return render(request, 'phones/addpage.html', context=context)
 
 
 def login(request):
@@ -66,4 +86,3 @@ def show_brand(request, brand_slug):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена<h1>')
-
