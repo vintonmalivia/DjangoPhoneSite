139311f0b6fd774d3855e816ceb1b18e9158d947
Anton Shvechikhin <antonkamski@gmail.com>
2023-05-30 00:06:36 +0400

    Using model forms, created validator

--- phones/forms.py
+++ phones/forms.py
@@ -1,10 +1,25 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import *
 
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length=255, label='URL')
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Контент')
-    is_published = forms.BooleanField(label='Публикация', required=False, initial=True)
-    brand = forms.ModelChoiceField(queryset=Brand.objects.all(), label='Бренд', empty_label='Бренд не выбран')
+class AddPostForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['brand'].empty_label = 'Брэнд не выбран'
+
+    class Meta:
+        model = Phone
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'brand']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10})
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов')
+        return title
